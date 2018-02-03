e633a6c841509c51f671d1c034ecc99c56b93c03
ulz <ulz@ATTHL009.SATTLER.AG>
2018-02-03 10:51:07 +0100

    Anpassung LED Buttons

--- swe-iot/src/main/java/org/c02/swe/iot/demos/LedDemoApp.java
+++ swe-iot/src/main/java/org/c02/swe/iot/demos/LedDemoApp.java
@@ -1,8 +1,41 @@
 package org.c02.swe.iot.demos;
 
+import java.awt.Color;
+
+import org.c02.swe.iot.Button;
+import org.c02.swe.iot.ButtonConnection;
+import org.c02.swe.iot.cloud.api.IParticleApi;
+import org.c02.swe.iot.cloud.api.ParticleApi;
+
 public class LedDemoApp {
 
+	static IParticleApi api = new ParticleApi(new ButtonConnection());
+
 	public static void main(String[] args) {
+		Button bt = new Button(api);
+
+		for (int i = 6; i < args.length; i++) {
+
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException ex) {
+				Thread.currentThread().interrupt();
+			}
+			bt.setLed(i, Color.green);
+		}
+
+		// bt.allLedsOff();
+
+		for (int i = 6; i < args.length; i++) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException ex) {
+				Thread.currentThread().interrupt();
+			}
+			bt.setLed(i, Color.green);
+		}
+
+		bt.allLedsOff();
 
 	}
 
